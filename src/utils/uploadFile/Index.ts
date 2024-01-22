53a0d974d63c3edd51739cc8a2de120b8454f063
Omer <omery020040@gmail.com>
2024-01-22 20:16:13 +0300

    update

--- src/utils/uploadFile/Index.ts
+++ src/utils/uploadFile/Index.ts
@@ -1,12 +1,31 @@
 import multer from "multer";
+import { SomeThingWentWrongError } from "../../errors/something-went-wrong";
 const upload = multer({
   limits: {
-    fileSize: 10000000000000000,
+    fileSize: 10 * 1024 * 1024, // 10 MB in bytes
   },
   fileFilter(req: any, file: any, cb: any) {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error("Please upload a valid image file"));
+      return cb(
+        new SomeThingWentWrongError([
+          {
+            reason: "File must be a image file",
+            statusCode: 404,
+          },
+        ])
+      );
     }
+    if (file.size > 10 * 1024 * 1024) {
+      return cb(
+        new SomeThingWentWrongError([
+          {
+            reason: "File size exceeds the allowed limit",
+            statusCode: 404,
+          },
+        ])
+      );
+    }
+
     cb(undefined, true);
   },
 });
